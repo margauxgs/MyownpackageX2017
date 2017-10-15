0d212a081b4ce797903ee0473474ed07351e1166
Margaux Girard-Soppet <margaux.girard-soppet@polytechnique.edu>
2017-10-15 17:07:11 +0200

    Creating a R file with the thrid function I want to have in the package: multi_excel

---
+++ R/multi_excel.R
@@ -0,0 +1,18 @@
+#' Import all sheets from an xlsx file
+#'
+#' @param file the path to the excel file we want to import the sheets from
+#'
+#' @return a list
+#' @export
+#' @import readxl
+#' @examples
+#' \dontrun{
+#' multi_excel("/path/to/file")
+#' }
+multi_excel <- function(file) {
+  all_sheets <- readxl::excel_sheets(file)
+  result <- lapply(all_sheets, function(sheet) {
+    readxl::read_excel(file, sheet = sheet)
+  })
+  return(result)
+}
